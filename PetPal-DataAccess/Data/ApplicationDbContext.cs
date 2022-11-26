a465da3e801d6e21081c4da6f178a23ccc8c4ee0
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-11-26 21:14:29 +0100

    Add project files.

---
+++ PetPal-DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetPal_DataAccess.Models;
+
+namespace PetPal_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ PetPal-DataAccess/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace PetPal_DataAccess.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+    }
+}
