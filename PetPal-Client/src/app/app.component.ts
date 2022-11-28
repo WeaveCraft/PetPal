d402acce9003d30119a4dafef162c129fe63bfe5
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-11-28 23:20:03 +0100

    Working on Angular Skeleton.

--- PetPal-Client/src/app/app.component.ts
+++ PetPal-Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PetPal-Client';
+  title = 'Pet Pal';
+
+  constructor(private http: HttpClient){
+
+  }
+
 }
