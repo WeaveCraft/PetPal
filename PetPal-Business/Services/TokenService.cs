ddf80e61ff33d1830bb0fce8e84876024d6c7ca3
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-11-30 15:30:19 +0100

    CreateToken method added.

--- PetPal-Business/Services/TokenService.cs
+++ PetPal-Business/Services/TokenService.cs
@@ -1,13 +1,41 @@
-﻿using PetPal_Business.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PetPal_Business.Interfaces;
 using PetPal_DataAccess.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace PetPal_Business.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7), //Token Expires after one week, and after that we need to login again to get another token.
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
