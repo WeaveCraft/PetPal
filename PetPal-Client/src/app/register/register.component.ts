3e988a317c2aa13e1fdc4fb29a438b5b9a265ad4
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-12-02 09:53:13 -0800

    Able to register new users.

--- PetPal-Client/src/app/register/register.component.ts
+++ PetPal-Client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-register',
@@ -6,17 +8,21 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe ({
+      next: () => {
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
