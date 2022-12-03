ec7df279fa1c26b34b2b1ef50a5262264cc1d6b2
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-12-03 11:10:20 -0800

    Added more detailed error handling.

--- PetPal-Client/src/app/nav/nav.component.ts
+++ PetPal-Client/src/app/nav/nav.component.ts
@@ -20,7 +20,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: () => 
         this.routers.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
     })
   }
 
