38027c86c6d218d6652567318de8fd7bec4f1c23
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-12-03 10:56:31 -0800

    Fixing error handling Client Side. Added server-rror component with not-found component.

--- PetPal-Client/src/app/errors/test-error/test-error.component.ts
+++ PetPal-Client/src/app/errors/test-error/test-error.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.css']
 })
 export class TestErrorComponent implements OnInit {
-  baseUrl = 'https://localhost:7025/api/'
+  baseUrl = 'https://localhost:7025/api/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -45,7 +46,10 @@ export class TestErrorComponent implements OnInit {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        this.validationErrors = error;
+      }
     })
   }
 
