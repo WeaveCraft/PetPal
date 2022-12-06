5dd86a938207919262bdebe3efbb2bb023a6f5f1
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-12-06 14:45:22 -0800

    Started working on displaying users.

--- PetPal-Client/src/app/_models/member.ts
+++ PetPal-Client/src/app/_models/member.ts
@@ -1,20 +1,17 @@
 import { Animal } from './Animal';
 
-declare module namespace {
-
-    export interface Member {
-        id: number;
-        username: string;
-        age: number;
-        created: Date;
-        lastActive: Date;
-        introduction: string;
-        lookingFor: string;
-        city: string;
-        country: number;
-        language: number;
-        animals: Animal[];
-    }
-
+export interface Member {
+    id: number;
+    username: string;
+    age: number;
+    created: Date;
+    lastActive: Date;
+    introduction: string;
+    lookingFor: string;
+    city: string;
+    country: number;
+    language: number;
+    animals: Animal[];
 }
+
 
