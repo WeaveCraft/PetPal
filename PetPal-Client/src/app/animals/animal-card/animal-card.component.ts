6331f6f641c8521355189b83f5b6289721e4700e
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-12-11 05:06:21 +0100

    Working on styling for pets card.

--- PetPal-Client/src/app/animals/animal-card/animal-card.component.ts
+++ PetPal-Client/src/app/animals/animal-card/animal-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { Animal } from 'src/app/_models/animal';
 
 @Component({
   selector: 'app-animal-card',
   templateUrl: './animal-card.component.html',
-  styleUrls: ['./animal-card.component.css']
+  styleUrls: ['./animal-card.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AnimalCardComponent implements OnInit {
   @Input() animal: Animal | undefined;
