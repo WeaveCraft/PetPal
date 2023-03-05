0c3c0d6beb0e17b24e4ef5d917812edd0d8d950b
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-03-05 17:42:47 +0100

    Working on edit profile

--- PetPal-Client/src/app/animals/animal-edit/animal-edit.component.ts
+++ PetPal-Client/src/app/animals/animal-edit/animal-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
 import { Animal } from 'src/app/_models/animal';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
@@ -11,13 +12,23 @@ import { AnimalsService } from 'src/app/_services/animals.service';
 })
 export class AnimalEditComponent implements OnInit {
   animal: Animal | undefined;
-  user: User | undefined;
+  user: User | null = null;
 
   constructor(private accountService: AccountService, private animalService: AnimalsService) { 
-    this.accountService.currentUser$.pipe(take(1))
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => this.user = user
+    })
   }
 
   ngOnInit(): void {
+    this.loadAnimal();
   }
+
+  loadAnimal() {
+    if(!this.user) return;
+    this.animalService.getAnimal(this.user.username).subscribe({
+      next: animal => this.animal = animal
+  })
+}
 
 }
