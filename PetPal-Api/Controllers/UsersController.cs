408a4d612e7e3f6d5693b6ed286a3797e32c5487
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-03-25 04:19:00 +0100

    Added controller for updating users.

--- PetPal-Api/Controllers/UsersController.cs
+++ PetPal-Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PetPal_Business.Repositories.Interfaces;
 using PetPal_Model.DTOs;
+using System.Security.Claims;
 
 namespace PetPal_Api.Controllers
 {
@@ -30,6 +31,22 @@ namespace PetPal_Api.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if (await _userRepository.SaveAllAsync()) 
+               return NoContent();
+
+            return BadRequest("Error in updating member!");
         }
     }
 }
---
+++ PetPal-Model/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using PetPal_Model.Enums;
+
+namespace PetPal_Model.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string City { get; set; }
+        public Country? Country { get; set; }
+    }
+}
