2933e7d60c024b0479c077286e43ca9a4a1c6ce7
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-04-01 13:50:02 +0200

    Added DeletePhoto endpoint.

--- PetPal-Api/Controllers/UsersController.cs
+++ PetPal-Api/Controllers/UsersController.cs
@@ -104,5 +104,29 @@ namespace PetPal_Api.Controllers
 
             return BadRequest("Problem with choosing main photo");
         }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("Not possible to delete main photo.");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("There was a problem when deleting the photo");
+        }
     }
 }
