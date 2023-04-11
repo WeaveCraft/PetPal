9a19efa9faeedd7c2401101fe036a05915031903
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-04-11 18:27:59 +0200

    feat: members can like each other

---
+++ PetPal-Api/Controllers/LikesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetPal_Business.Extensions;
+using PetPal_Business.Repositories.Interfaces;
+using PetPal_Model.DTOs;
+using PetPal_Model.Models;
+
+namespace PetPal_Api.Controllers
+{
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _userRepository = userRepository;
+            _likesRepository = likesRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = int.Parse(User.GetUserId());
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+            if (sourceUser.Username == username) return BadRequest("Not able to like yourself");
+
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You have already like this member");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Couldn't like a member");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await _likesRepository.GetUserLikes(predicate, int.Parse(User.GetUserId()));
+            return Ok(users);
+        }
+
+    }
+}
