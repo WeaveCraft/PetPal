84bfa55d77fc3250a4ea5230ae57be42c7eea5fa
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-04-11 19:13:52 +0200

    feat: pagination for frontend.

--- PetPal-Client/src/app/lists/lists.component.ts
+++ PetPal-Client/src/app/lists/lists.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MembersService } from '../_services/members.service';
 import { Member } from '../_models/member';
+import { Pagination } from '../_models/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -9,7 +10,10 @@ import { Member } from '../_models/member';
 })
 export class ListsComponent implements OnInit {
   members: Member[] | undefined;
+  pagination: Pagination |undefined;
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MembersService) { }
 
@@ -18,11 +22,19 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.memberService.getLikes(this.predicate).subscribe({
+    this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe({
       next: response => {
-        this.members = response
+        this.members = response.result;
+        this.pagination = response.pagination;
       }
     })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
   }
 
 }
