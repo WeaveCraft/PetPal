17347eb20c42bd3d6d5971394f10a479866e695b
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-04-15 09:08:34 +0200

    feat: can see messages thread in users.

---
+++ PetPal-Client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+  @Input() username: string | undefined;
+  messages: Message[] = [];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    if (this.username){
+      this.messageService.getMessageThread(this.username).subscribe({
+        next: messages => this.messages = messages
+      })
+    }
+  }
+
+}
