8e6bba5a47a55c7f90928e10ff56bdc1141086a2
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-04-17 09:30:16 +0200

    wip 1: styling. Navbar & List -Component.

--- PetPal-Client/src/app/nav/nav.component.ts
+++ PetPal-Client/src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
   model: any = {}
